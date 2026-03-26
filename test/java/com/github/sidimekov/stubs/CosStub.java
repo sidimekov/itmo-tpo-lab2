d5b330cf1c1a58b7af9be5150d4e0d0a1f7a60b8
leias19 <liza0272297@yandex.ru>
2026-03-26 20:15:59 +0300

    просто поменяла название каталога

---
+++ test/java/com/github/sidimekov/stubs/CosStub.java
@@ -0,0 +1,23 @@
+package java.com.github.sidimekov.stubs;
+
+import com.github.sidimekov.AbstractFunction;
+
+import java.util.Map;
+
+public class CosStub extends AbstractFunction {
+    private final Map<Double, Double> table;
+
+    public CosStub(Map<Double, Double> table) {
+        this.table = table;
+    }
+
+    @Override
+    protected String getFunctionName() {
+        return "CosStub";
+    }
+
+    @Override
+    public double compute(double x) {
+        return this.table.get(x);
+    }
+}
